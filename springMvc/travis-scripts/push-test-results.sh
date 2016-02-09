bf5dd7790c9f2abcd945e4f033bb693f249f449d
cg250 <cg250@student.le.ac.uk>
2016-02-09 01:57:11 +0000

    Travis auto commit fix

--- springMvc/travis-scripts/push-test-results.sh
+++ springMvc/travis-scripts/push-test-results.sh
@@ -17,8 +17,7 @@ git reset upstream/master
 touch .
 
 # Add the reports
-cd ../travis-build
-#git add -A cucumber-html-report
+git add -A cucumber-html-report
 git add -A reports
 git add -A docs
 git add -A libs
