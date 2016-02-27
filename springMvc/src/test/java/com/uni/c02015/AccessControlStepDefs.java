81ebd62e6d0070ae251c74bc3de064476844a37e
majidhussainleicester <hussainmajid95@gmail.com>
2016-02-27 18:44:09 +0000

    added comments due to style check errorsW

--- springMvc/src/test/java/com/uni/c02015/AccessControlStepDefs.java
+++ springMvc/src/test/java/com/uni/c02015/AccessControlStepDefs.java
@@ -1,13 +1,9 @@
 package com.uni.c02015;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-
-import javax.servlet.Filter;
-
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,10 +13,13 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import javax.servlet.Filter;
+
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class AccessControlStepDefs {
 
@@ -32,7 +31,7 @@ public class AccessControlStepDefs {
   private MockMvc mockMvc;
   private ResultActions result;
   private Authentication authentication;
-
+  //This is ran every scenario
   @Before
   public void setup() {
     this.mockMvc = MockMvcBuilders
@@ -41,41 +40,36 @@ public class AccessControlStepDefs {
             .apply(springSecurity())
             .build();
   }
-
+  //Creates an authentication token using username, password and role
   @Given("^I am a \"([^\"]*)\" with username \"([^\"]*)\" and password \"([^\"]*)\"$")
   public void i_am_a_with_username_and_password(String arg1, String arg2, String arg3)
           throws Throwable {
     authentication = new UsernamePasswordAuthenticationToken(arg2, arg3,
             AuthorityUtils.createAuthorityList("ROLE_" + arg1));
   }
-
-
+  //When you enter credentials in login
   @When("^I access \"([^\"]*)\"$")
   public void i_access(String arg1) throws Throwable {
     result = mockMvc.perform(get(arg1).with(authentication(authentication)));
   }
-
+  //Checks your role
   @Then("^My authentication is true with role \"([^\"]*)\"$")
   public void my_authentication_is_true_with_role(String arg1) throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
     result.andExpect(authenticated().withRoles(arg1));
   }
-
+  //If not authorized you will be redirected to login
   @Then("^My authentication is false with role \"([^\"]*)\"$")
   public void my_authentication_is_false_with_role(String arg1) throws Throwable {
     result.andExpect(redirectedUrl("https://localhost/login-form"));
   }
-
+  //Need to implement these
   @Given("^I am an authenticated \"([^\"]*)\" with username \"([^\"]*)\"$")
   public void i_am_an_authenticated_with_username(String arg1, String arg2) throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    //throw new PendingException();
   }
-
+  //Need to implement these
   @Then("^My authentication is <isAuth> with role \"([^\"]*)\"$")
   public void my_authentication_is_isAuth_with_role(String arg1) throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    //throw new PendingException();
+
   }
 
 }
