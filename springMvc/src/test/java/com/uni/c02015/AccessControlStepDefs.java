1c9fb2ee03a7897a18ea58da918aeeb54eba5321
ahtif <ahtif.a@hotmail.co.uk>
2016-02-27 19:54:23 +0000

    fixed more checkstyle errors

--- springMvc/src/test/java/com/uni/c02015/AccessControlStepDefs.java
+++ springMvc/src/test/java/com/uni/c02015/AccessControlStepDefs.java
@@ -1,9 +1,12 @@
 package com.uni.c02015;
 
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,13 +16,13 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import javax.servlet.Filter;
 
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class AccessControlStepDefs {
 
@@ -31,7 +34,10 @@ public class AccessControlStepDefs {
   private MockMvc mockMvc;
   private ResultActions result;
   private Authentication authentication;
-  //This is ran every scenario
+
+  /**
+   * Ran before every scenario.
+   */
   @Before
   public void setup() {
     this.mockMvc = MockMvcBuilders
@@ -42,14 +48,14 @@ public class AccessControlStepDefs {
   }
   //Creates an authentication token using username, password and role
   @Given("^I am a \"([^\"]*)\" with username \"([^\"]*)\" and password \"([^\"]*)\"$")
-  public void i_am_a_with_username_and_password(String arg1, String arg2, String arg3)
+  public void iam_a_with_username_and_password(String arg1, String arg2, String arg3)
           throws Throwable {
     authentication = new UsernamePasswordAuthenticationToken(arg2, arg3,
             AuthorityUtils.createAuthorityList("ROLE_" + arg1));
   }
   //When you enter credentials in login
   @When("^I access \"([^\"]*)\"$")
-  public void i_access(String arg1) throws Throwable {
+  public void access(String arg1) throws Throwable {
     result = mockMvc.perform(get(arg1).with(authentication(authentication)));
   }
   //Checks your role
@@ -64,7 +70,7 @@ public class AccessControlStepDefs {
   }
   //Need to implement these
   @Given("^I am an authenticated \"([^\"]*)\" with username \"([^\"]*)\"$")
-  public void i_am_an_authenticated_with_username(String arg1, String arg2) throws Throwable {
+  public void authenticated_with_username(String arg1, String arg2) throws Throwable {
   }
   //Need to implement these
   @Then("^My authentication is <isAuth> with role \"([^\"]*)\"$")
