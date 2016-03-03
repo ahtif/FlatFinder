4d67754d3c7e9a497b6c782f9239300c30d89126
cg250 <cg250@student.le.ac.uk>
2016-03-03 15:30:42 +0000

    Property Controller

--- springMvc/src/main/java/com/uni/c02015/controller/PropertyController.java
+++ springMvc/src/main/java/com/uni/c02015/controller/PropertyController.java
@@ -1,8 +1,21 @@
 package com.uni.c02015.controller;
 
+import com.uni.c02015.domain.Landlord;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PropertyController {
 
+  @RequestMapping(value = "/property/add", method = RequestMethod.GET)
+  public ModelAndView showLandlordRegistrationForm(HttpServletRequest request) {
+
+    ModelAndView modelAndView = new ModelAndView("property/add");
+
+    return modelAndView;
+  }
 }
